9bcb589277a168e3c4f889e328cd12047b3ed23c
mfazel <mfazel@techfak.de>
2022-09-23 10:19:08 +0200

    fuction that cods the input String with pushing each letter to the next 13. letter.

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,3 +1,33 @@
-public class Encryption {
+import java.util.List;
 
+public class Encryption {
+    public static void main(String[] args) {
+        System.out.println(stringCoder("Hello"));
+    }
+
+    /*
+shows the poision of letter in alphabet
+     */
+    private static int positionOfChar(Character ch) {
+        return (int) ch;
+    }
+
+    /*
+ pushes each letter for 13 letters
+       */
+    private static String stringCoder(String input) {
+        String output = "";
+        char tempLetter = ' ';
+        for (int i = 0; i < input.length(); i++) {
+            if (positionOfChar(input.charAt(i)) <= 109) {
+                tempLetter = (char) ((char) positionOfChar(input.charAt(i)) + 13);
+                output += tempLetter;
+            } else if (positionOfChar(input.charAt(i)) > 109) {
+                tempLetter = (char) ((char) 96 + positionOfChar(input.charAt(i)) - 109);
+                output += tempLetter;
+            }
+        }
+
+        return output;
+    }
 }
